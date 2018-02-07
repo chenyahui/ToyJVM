a17ba3d16ec245d55efdef0524dfe5837bbb0a4e
chenyahui <805356546@qq.com>
2018-02-07 13:14:02 +0800

    add native method: java/lang/Class->getPrimitiveClass

--- src/native/get_primitive_class.cpp
+++ src/native/get_primitive_class.cpp
@@ -1,4 +1,5 @@
 #include <jvm/native/get_primitive_class.h>
+#include <jvm/rtdata/class_loader.h>
 #include <jvm/rtdata/jvm_frame.h>
 #include <jvm/rtdata/string_pool.h>
 
@@ -9,5 +10,8 @@ void getPrimitiveClass(JFrame* frame)
     auto name_obj = frame->LocalVars().Get<JObject*>(0);
     auto name = TransJString(name_obj);
     auto loader = name_obj->jclass()->class_loader();
+    auto jlc_obj = loader->LoadClass(name)->jl_class();
+
+    frame->OpStack().Push<JObject*>(jlc_obj);
 }
 }
